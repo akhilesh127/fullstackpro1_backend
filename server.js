bc6fe73cd57bea2306aeb57162fa24d75754f282
akhilesh127 <112255923+akhilesh127@users.noreply.github.com>
2025-11-14 15:08:09 +0530

    adding

--- server.js
+++ server.js
@@ -1,18 +1,14 @@
  import express from 'express';
- import mongoose from 'mongoose';
- import cors from 'cors';
  import dotenv from 'dotenv';
+ import mongoose from 'mongoose';
+    import cors from 'cors'; 
  dotenv.config();
  
 
-
-const express=require('express');
-const mongoose=require('mongoose');
-const cors =require('cors');
 const app=express();
 app.use(cors());
 app.use(express.json());
-mongoose.connect(process.env.MONGO_URL,{)
+mongoose.connect(process.env.MONGO_URL)
 .then(()=>{
     console.log("Connected to MongoDB");
 })
